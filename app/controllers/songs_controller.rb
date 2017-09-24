d64474bbc42e8d9fc9745cd86cf8e74e0924f6e9
Raul <ing.raulreyes@gmail.com>
2017-09-24 13:12:58 -0700

    lista las canciones correctamente

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,26 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new(params[:song])
   end
+
+  def create
+    @song = Song.new(params[:song])
+    @song.save
+    redirect_to root_path
+  end
+
+  def show
+   @song = Song.find(params[:id])
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:titulo)
+  end
+
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @songs = Song.all
   end
 end
